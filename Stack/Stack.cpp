b008ffa40a7c11f62adf0f3966d3359552387611
Houston Brown <80162431+teagist@users.noreply.github.com>
2024-10-24 10:24:19 -0500

    added formatting to search function - HDB

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -108,10 +108,8 @@ int Stack<T>::Size() const
 }
 
 
-
-
 //***************************************************************************
-//		Stack method that will return NULL if the stack is empty, or if the
+//	    Stack method that will return NULL if the stack is empty, or if the
 //	item is not found, after displaying a message to the user.  If the item 
 //	is found, then a message will be displayed and the pointer to the 
 //	location returned.    
@@ -119,32 +117,32 @@ int Stack<T>::Size() const
 //	Parameters:
 //		item: the element to be searched for
 //***************************************************************************
-
 template<class T>
 StackNode<T>* Stack<T>::Search(T item)const
 {
-	bool success = false;
-	StackNode<T>* temp = head;
-	
-	if (temp == NULL)
-		return NULL;
+    bool success = false;
+    StackNode<T>* temp = head;
+
+    if (temp == NULL)
+    {
+	return NULL;
+    }
 		
-	while(temp != NULL)
+    while(temp != NULL)
+    {
+	if(temp->data == item)
 	{
-		if(temp->data == item)
-		{
-			success = true;
-			cout << item << " was found." << endl;
-		}
-		temp = temp -> next;
+	    success = true;
+	    cout << item << " was found." << endl;
 	}
+	temp = temp -> next;
+    }
 
-	if(success == false)
-	{
-		cerr << item << " was not found." << endl;
+    if(success == false)
+    {
+	cerr << item << " was not found." << endl;
         return NULL;
-	}
-
+    }
     return temp;
 }
 
